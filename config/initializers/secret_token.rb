c49ec807ad237161e35fc9e20c85eadaee94ae7c
Tatsuro Baba <harakirisoul@gmail.com>
2011-08-23 12:47:01 +0900

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = '169941ce521a07122e4589dbf5f9e314109daa630115444998d816e165e0077d8fc5460149f9c3b7ad16d07ed26078b63132807264b3e7374531548f18628c49'
